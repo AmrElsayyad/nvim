52c33d01d43a93ef0e0b808226c7b82ecf2d0c43
Amr Elsayyad <20210190+AmrElsayyad@users.noreply.github.com>
2025-06-06 22:57:37 +0300

    feat: Add catppuccin theme and enable huez manager import
    Signed-off-by: Amr Elsayyad <20210190+AmrElsayyad@users.noreply.github.com>

--- lua/plugins/huez.lua
+++ lua/plugins/huez.lua
@@ -1,7 +1,7 @@
 return {
     "vague2k/huez.nvim",
     -- if you want registry related features, uncomment this
-    -- import = "huez-manager.import"
+    import = "huez-manager.import",
     branch = "stable",
     event = "UIEnter",
     config = function()
