d1b12efb16e5f775c8a59052a215cede2f693568
Amr Elsayyad <20210190+AmrElsayyad@users.noreply.github.com>
2025-08-07 21:34:35 +0300

    feat(plugins): migrate to obsidian-nvim/obsidian.nvim and clean up config
    - Switch plugin source from epwalsh/obsidian.nvim to obsidian-nvim/obsidian.nvim
    
    - Remove unused dependencies (pomo.nvim, dataview.nvim) from obsidian config
    
    - Update daily notes config for correct date format and template path
    
    - Update lock file and spell files for recent plugin and word additions
    
    - Update avanterules documentation for Context7 MCP documentation lookup
    
    This modernizes the Obsidian plugin setup and improves reliability.
    
    Signed-off-by: Amr Elsayyad <20210190+AmrElsayyad@users.noreply.github.com>

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,5 +1,5 @@
 return {
-    "epwalsh/obsidian.nvim",
+    "obsidian-nvim/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
     lazy = true,
     ft = "markdown",
@@ -19,8 +19,6 @@ return {
         "hrsh7th/nvim-cmp",
         "nvim-telescope/telescope.nvim",
         "nvim-treesitter",
-        "epwalsh/pomo.nvim",
-        "hisbaan/dataview.nvim",
     },
     opts = {
         workspaces = {
@@ -35,13 +33,13 @@ return {
             -- Optional, if you keep daily notes in a separate directory.
             folder = "Timestamps",
             -- Optional, if you want to change the date format for the ID of daily notes.
-            date_format = "YYYY/MM-MMMM/YYYY-MM-DD-dddd",
+            date_format = "%Y/%m-%B/%Y-%m-%d-%A",
             -- Optional, if you want to change the date format of the default alias of daily notes.
             -- alias_format = "%B %-d, %Y",
             -- Optional, default tags to add to each new daily note created.
             -- default_tags = { "daily-notes" },
             -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-            template = "Templates/Daily Notes",
+            template = "Templates/Daily Notes.md",
         },
 
         templates = {
