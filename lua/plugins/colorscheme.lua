6965dfa29b38a2e20dea1a784a11b265d1f4cf9c
Amr Elsayyad <20210190+AmrElsayyad@users.noreply.github.com>
2025-09-26 21:30:15 +0300

    style(colorscheme): update theme to day, add nightfox.nvim, and update plugin versions
    Changed default colorscheme style to day, added nightfox.nvim to plugins, and updated plugin versions in lazy-lock.json for improved visuals and stability.
    
    Signed-off-by: Amr Elsayyad <20210190+AmrElsayyad@users.noreply.github.com>

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,12 +5,7 @@ return {
         priority = 1000,
         opts = {
             -- The theme comes in four flavors: night, storm, day, and moon.
-            style = "night",
-
-            on_highlights = function(highlights, colors)
-                highlights.WinSeparator = { fg = colors.teal }
-                highlights.AvanteSidebarWinSeparator = { fg = colors.teal }
-            end,
+            style = "day",
         },
     },
     {
@@ -31,6 +26,7 @@ return {
             flavour = "latte", -- latte, frappe, macchiato, mocha
         },
     },
+    { "EdenEast/nightfox.nvim" },
     {
         "LazyVim/LazyVim",
         opts = {
