c7b29249f2a018eb070b30d5c3c31902764b9d0d
Amr Elsayyad <20210190+AmrElsayyad@users.noreply.github.com>
2026-07-11 18:58:26 +0300

    feat(whitespace): enable strip-on-save for vim-better-whitespace
    Configure vim-better-whitespace to strip trailing whitespace on save.

--- lua/plugins/whitespace.lua
+++ lua/plugins/whitespace.lua
@@ -1,4 +1,12 @@
 return {
-    "ntpeters/vim-better-whitespace",
-    event = "BufRead",
+  "ntpeters/vim-better-whitespace",
+  event = "BufRead",
+  init = function()
+    vim.g.strip_whitespace_on_save = 1
+    vim.g.strip_whitespace_confirm = 0
+    vim.g.strip_max_file_size = 0
+    vim.g.better_whitespace_filetypes_blacklist = {
+      "diff", "git", "gitcommit", "unite", "qf", "help", "markdown", "fugitive",
+    }
+  end,
 }
